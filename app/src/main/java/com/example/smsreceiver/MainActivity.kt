e7686208eac542d129b8a66ad933f25317208bbe
Gilang <gilangnurrizki173@gmail.com>
2024-10-02 18:55:00 +0700

    update logic

--- app/src/main/java/com/example/smsreceiver/MainActivity.kt
+++ app/src/main/java/com/example/smsreceiver/MainActivity.kt
@@ -2,26 +2,41 @@ package com.example.smsreceiver
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.smsreceiver.databinding.ActivityMainBinding
+import android.Manifest
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private var binding: ActivityMainBinding? = null
+
+    var requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
         binding?.btnPermission?.setOnClickListener(this)
+
+
     }
 
     override fun onClick(v: View?) {
        when (v?.id) {
-
+            R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
        }
     }
 
